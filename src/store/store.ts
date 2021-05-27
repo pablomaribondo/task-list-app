75d4231ac45691818265a6d38d8afaf086e5106f
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-27 12:06:37 -0300

    feat: store created using redux

---
+++ src/store/store.ts
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default store;
