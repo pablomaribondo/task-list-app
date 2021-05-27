00ec577d374054deab3732b27b64ed61ee2d0557
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-27 15:34:34 -0300

    feat(listReducer): helper functions & some cases added

--- src/store/reducers/listReducer.ts
+++ src/store/reducers/listReducer.ts
@@ -1,6 +1,17 @@
-import { ListsAction, ListState } from '../types';
+import {
+  ADD_LIST,
+  DELETE_LIST,
+  GET_LISTS,
+  GET_LIST_BY_ID,
+  Lists,
+  ListsAction,
+  ListState,
+  SET_LIST_TO_DELETE,
+  SET_LIST_TO_EDIT,
+  UPDATE_LIST
+} from '../types';
 
-const initialState: ListState = {
+const INITIAL_STATE: ListState = {
   lists: {},
   listToDelete: '',
   listToEdit: null,
@@ -10,9 +21,99 @@ const initialState: ListState = {
   taskToEdit: null
 };
 
+const LS_TAG = 'task_list';
+
+const getListsFromLS = (): Lists => {
+  if (localStorage.getItem(LS_TAG)) {
+    return JSON.parse(localStorage.getItem(LS_TAG) || '{}');
+  }
+
+  return {};
+};
+
+const saveListsToLS = (lists: Lists) => {
+  localStorage.setItem(LS_TAG, JSON.stringify(lists));
+};
+
 // eslint-disable-next-line default-param-last
-export default (state = initialState, action: ListsAction): ListState => {
+export default (state = INITIAL_STATE, action: ListsAction): ListState => {
+  const listsFromLS = getListsFromLS();
+
   switch (action.type) {
+    case ADD_LIST: {
+      const clonedListsFromLS = { ...listsFromLS };
+      clonedListsFromLS[action.payload.id] = action.payload;
+
+      saveListsToLS(clonedListsFromLS);
+
+      return {
+        ...state,
+        lists: clonedListsFromLS
+      };
+    }
+
+    case GET_LISTS:
+      return {
+        ...state,
+        lists: listsFromLS
+      };
+
+    case GET_LIST_BY_ID: {
+      const list = listsFromLS[action.payload];
+
+      return {
+        ...state,
+        listById: list
+      };
+    }
+
+    case SET_LIST_TO_DELETE:
+      return {
+        ...state,
+        listToDelete: action.payload
+      };
+
+    case SET_LIST_TO_EDIT: {
+      const listToEdit = listsFromLS[action.payload];
+
+      return {
+        ...state,
+        listToEdit
+      };
+    }
+
+    case DELETE_LIST: {
+      const clonedListsFromLS = { ...listsFromLS };
+      const listId = clonedListsFromLS[action.payload].id;
+
+      delete clonedListsFromLS[action.payload];
+      saveListsToLS(clonedListsFromLS);
+
+      return {
+        ...state,
+        lists: clonedListsFromLS,
+        listToDelete: '',
+        listById: null,
+        selectedList:
+          state.selectedList && listId === state.selectedList.id
+            ? null
+            : state.selectedList
+      };
+    }
+
+    case UPDATE_LIST: {
+      const clonedListsFromLS = { ...listsFromLS };
+
+      clonedListsFromLS[action.payload.id].name = action.payload.name;
+      saveListsToLS(clonedListsFromLS);
+
+      return {
+        ...state,
+        lists: clonedListsFromLS,
+        listToEdit: null
+      };
+    }
+
     default:
       return state;
   }
