d8971d3f98e44ab5eed4cb5adcc64da7860b676b
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-28 20:30:38 -0300

    feat: store & styles available at application entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import '../node_modules/bulma/css/bulma.min.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+
+import store from './store/store';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
