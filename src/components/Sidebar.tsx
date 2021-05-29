78272ed0aab8e64c26b6f1958e97002e748fbaed
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-29 10:57:18 -0300

    feat: lists component created

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 
 import CreateNewList from './CreateNewList';
+import Lists from './Lists';
 
 const Sidebar: FC = () => {
   return (
     <div className="column is-3">
       <CreateNewList />
+      <Lists />
     </div>
   );
 };
