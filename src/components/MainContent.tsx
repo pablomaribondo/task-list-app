c7bec73a35e2acd4119eeae3f7f10908c66a3137
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-08 10:13:34 -0300

    feat: create Tasks component

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import AddNewTask from './AddNewTask';
 import SelectList from './SelectList';
+import Tasks from './Tasks';
 
 const MainContent: FC = () => {
   const selectedList = useSelector(
@@ -14,7 +15,13 @@ const MainContent: FC = () => {
     <div className="column is-9">
       <div className="box">
         <SelectList />
-        {selectedList && <AddNewTask list={selectedList} />}
+        {selectedList && (
+          <>
+            <AddNewTask list={selectedList} />
+            <hr />
+            <Tasks tasks={selectedList.tasks} />
+          </>
+        )}
       </div>
     </div>
   );
